971f098c0bef726d5d8c5b58969e4889dd1d5531
Liam Sean Brady <liamseanbrady@gmail.com>
2015-04-19 21:39:10 -0400

    Add specs for #add_goal and #add_reason

--- spec/user_story_spec.rb
+++ spec/user_story_spec.rb
@@ -2,17 +2,41 @@ require_relative 'spec_helper'
 require 'user_story'
 
 describe 'UserStory' do
+  let(:user_story) { UserStory.new }
+
   describe '#add_actor' do
-    it 'add an invalid input message to error messages when the input is empty string' do
-      user_story = UserStory.new
+    it 'adds an invalid input message to error messages for the actor when the input is empty string' do
       user_story.add_actor('')
-      user_story.error_messages.wont_be_empty
+      user_story.error_messages[:actor].wont_be_empty
     end
 
-    it 'adds the name of the actor to the current user story if input is valid' do
-      user_story = UserStory.new
+    it 'assigns the name of the actor to the actor ivar' do
       user_story.add_actor('User')
       user_story.actor.must_equal 'User'
+    end
+  end
+
+  describe '#add_goal' do
+    it 'adds an invalid input message to error messages when the input is empty string' do
+      user_story.add_goal('')
+      user_story.error_messages[:goal].wont_be_empty
+    end
+
+    it 'assigns the description of the goal to the goal ivar' do
+      user_story.add_goal('do TDD')
+      user_story.goal.must_equal('do TDD')
+    end
+  end
+
+  describe '#add_reason' do
+    it 'adds an invalid input message to error messages when the input is empty string' do
+      user_story.add_reason('')
+      user_story.error_messages[:reason].wont_be_empty
+    end
+
+    it 'assigns the reason for the user story to the reason ivar' do
+      user_story.add_reason('I can limit the errors in my code')
+      user_story.reason.must_equal('I can limit the errors in my code')
     end
   end
 end
