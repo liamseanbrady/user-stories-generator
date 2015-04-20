67aa2fa456f92c535f80ff420ca0fc9a6a62d580
Liam Sean Brady <liamseanbrady@gmail.com>
2015-04-19 21:38:49 -0400

    Add implementation code

--- lib/user_story.rb
+++ lib/user_story.rb
@@ -1,19 +1,33 @@
 class UserStory
-  attr_reader :actor
+  attr_accessor :actor, :goal, :reason
 
   def initialize
     @invalid_input = {}
   end
 
   def add_actor(input)
-    if input.empty? || input.nil?
-      @invalid_input[:actor] = "The actor's name cannot be blank"
-    else
-      @actor = input 
-    end
+    add_section(:actor, input)
+  end
+
+  def add_goal(input)
+    add_section(:goal, input)
+  end
+  
+  def add_reason(input)
+    add_section(:reason, input)
   end
 
   def error_messages
     @invalid_input
+  end
+
+  private
+
+  def add_section(section_name, content)
+    if content.empty? || content.nil?
+      @invalid_input[section_name.to_sym] = "#{section_name} cannot be blank"
+    else
+      self.send("#{section_name.to_sym}=", content)
+    end
   end
 end
